cfa62fe7ef7e791357aa05a35c25085a913ea883
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-03-02 12:52:49 -0500

    Refactor CarouselModel

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,22 +1,51 @@
 import CarouselModel from "../components/Homepage/CarouselModel";
 import Gallery from "../components/Homepage/Gallery";
-import { modelList } from "../utils/models";
+import { getActiveFalse } from "../utils/models";
 import Landing from "../components/Homepage/Landing";
 import Loader from "../components/Loader";
 import useLoadGallery from "../hooks/useLoadGallery";
-import { useAppSelector } from "../hooks/useRedux";
+import { useAppSelector, useAppDispatch } from "../hooks/useRedux";
+import { onRemoveModel, onSetModel } from "../store/gallery/gallerySlice";
+import { useState } from 'react';
 
 const HomePage = () => {
+  const dispatch = useAppDispatch();
   const { loading, posts } = useAppSelector(state => state.gallery);
   const { loadMoreRef } = useLoadGallery();
+  const [modelList, setModelList] = useState(getActiveFalse());
+  
+
+  const onSelectModel = (model_id: string) => {
+    dispatch(onSetModel(model_id));
+    setModelList(
+      modelList.map(model => {
+        if (model.mode_id === model_id) {
+          model.active = true;
+        } else {
+          model.active = false;
+        }
+        return model;
+      })
+    )
+  };
+
+  const removerModel = (model_id: string) => {
+    dispatch(onRemoveModel());
+    setModelList(modelList.map(model => {
+        if (model.mode_id === model_id) {
+            model.active = false;
+        }
+        return model;
+    }));
+  }
 
   return (
     <main className="pt-16">
       <Landing />
       <CarouselModel
         models={modelList}
-        handleModel={() => {}}
-        removerModel={() => {}}
+        handleModel={onSelectModel}
+        removerModel={removerModel}
       />
       <Gallery posts={posts} />
       <div
