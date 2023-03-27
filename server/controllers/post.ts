37ac840429b2f90ab911403a45a1899846d86e0b
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-03-27 17:09:12 -0500

    update .env

--- server/controllers/post.ts
+++ server/controllers/post.ts
@@ -7,9 +7,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
   cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
   });
 
 export const getPosts = async (req: Request, res: Response) => {
