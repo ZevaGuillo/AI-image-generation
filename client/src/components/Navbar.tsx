c3d2316f7407e3403087adba2254c568b82e99e3
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-03-27 23:44:31 -0500

    add facebook button

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,12 +2,18 @@ import { Link } from "react-router-dom";
 import { useAppSelector } from "../hooks/useRedux";
 import { FcGoogle } from "react-icons/fc";
 import UserNavbar from "./UserNavbar";
+import { FaFacebook } from "react-icons/fa";
+
 
 const Navbar = () => {
   const { status } = useAppSelector(state => state.auth);
 
   const handleOnClick = () => {
     window.open(`${import.meta.env.VITE_SERVER}/api/v1/auth/google/callback`, "_self");
+  };
+
+  const handleFacebookOnClick = () => {
+    window.open(`${import.meta.env.VITE_SERVER}/api/v1/auth/facebook/callback`, "_self");
   };
 
   return (
@@ -22,14 +28,24 @@ const Navbar = () => {
         {status === "authenticated" ? (
           <UserNavbar />
         ) : status === "not-authenticated" ? (
-          <div
-            className="inline-block rounded-full gradient-google p-[2px] hover:text-white focus:outline-none focus:ring active:text-opacity-75"
-            onClick={handleOnClick}>
-            <span className="flex items-center gap-2 rounded-full bg-bgColor px-4 py-2 text-sm font-medium transition  hover:ease-out hover:bg-opacity-80">
-              <span className="hidden md:block">Sign in with google</span>
-              <FcGoogle className="text-base" />
-            </span>
-          </div>
+          <>
+            <div
+              className="inline-block rounded-full gradient-google p-[2px] hover:text-white focus:outline-none focus:ring active:text-opacity-75"
+              onClick={handleOnClick}>
+              <span className="flex items-center gap-2 rounded-full bg-bgColor px-4 py-2 text-sm font-medium transition  hover:ease-out hover:bg-opacity-80">
+                <span className="hidden md:block">Sign in with google</span>
+                <FcGoogle className="text-base" />
+              </span>
+            </div>
+            <div
+              className="inline-block rounded-full gradient-google p-[2px] hover:text-white focus:outline-none focus:ring active:text-opacity-75"
+              onClick={handleFacebookOnClick}>
+              <span className="flex items-center gap-2 rounded-full bg-bgColor px-4 py-2 text-sm font-medium transition  hover:ease-out hover:bg-opacity-80">
+                <span className="hidden md:block">Sign in with Facebook</span>
+                <FaFacebook className="text-base" />
+              </span>
+            </div>
+          </>
         ) : (
           <></>
         )}
