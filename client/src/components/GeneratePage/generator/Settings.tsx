c615b3aeb1906f280d627d2eda5472dd42565c70
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-04-04 20:18:50 -0500

    Configuring the new generator

---
+++ client/src/components/GeneratePage/generator/Settings.tsx
@@ -0,0 +1,7 @@
+export const Settings = () => {
+  return (
+    <div className="w-full h-full p-[0.1rem] gradient rounded-lg">
+      <div className="bg-bgColor w-full h-full rounded-lg p-2">Settings</div>
+    </div>
+  );
+};
--- client/src/pages/GeneratePage.tsx
+++ client/src/pages/GeneratePage.tsx
@@ -1,11 +1,13 @@
-import Generator from "../components/GeneratePage/Generator";
+
 import Hero from "../components/GeneratePage/Hero";
+import {Generator2} from "../components/GeneratePage/generator/Generator2";
 
 const GeneratePage = () => {
   return (
     <main className="pt-16">
       <Hero />
-      <Generator/>
+      {/* <Generator/> */}
+      <Generator2/>
     </main>
   );
 };
