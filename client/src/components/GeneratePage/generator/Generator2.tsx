28868b984adc2759a9da388f76e43f1888f97918
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-04-13 22:41:09 -0500

    refactor: change generator breakpoint to lo

--- client/src/components/GeneratePage/generator/Generator2.tsx
+++ client/src/components/GeneratePage/generator/Generator2.tsx
@@ -9,14 +9,14 @@ export const Generator2 = () => {
   return (
     <GeneratorProvider>
       <section className="bg-generator py-8 px-2 md:px-20  min-h-screen bg-opacity-50">
-        <div className="flex flex-col gap-4 md:grid md:gap-0 w-full h-full grid-cols-4 grid-rows-5">
-          <div className=" w-full h-full md:col-span-3">
+        <div className="flex flex-col gap-4 lg:grid lg:gap-0 w-full h-full grid-cols-4 grid-rows-5">
+          <div className=" w-full h-full lg:col-span-3">
             <SelectModel/>
           </div>
-          <div className=" w-full  md:row-span-5 md:col-span-1">
+          <div className=" w-full  lg:row-span-5 lg:col-span-1">
             <Settings />
           </div>
-          <div className=" w-full md:col-span-3 md:row-span-4">
+          <div className=" w-full lg:col-span-3 lg:row-span-4">
             <DisplayPanel/>
           </div>
         </div>
