996de146191ba59f581aeeadcaf9d1946d18758f
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-04-14 18:41:40 -0500

    feat: add Blurhash to server

---
+++ server/helpers/blurhash-from-url.ts
@@ -0,0 +1,57 @@
+// import fetch from "node-fetch";
+import { encode, decode } from "blurhash";
+import sharp from 'sharp';
+import sizeOf from "image-size";
+
+export interface IOptions {
+    size?: number;
+}
+
+export interface IInput {
+    url: string;
+    options?: IOptions;
+}
+export interface IOutput {
+    encoded: string;
+    width: number;
+    height: number;
+}
+
+export const blurhashFromURL = async (url: string, options: IOptions = {}) => {
+    const { size = 4 } = options;
+
+    const response = await fetch(url);
+    const arrayBuffer = await response.arrayBuffer();
+    console.log(arrayBuffer);
+    
+    const returnedBuffer = Buffer.from(arrayBuffer);
+
+    const { width, height, } = sizeOf(returnedBuffer);
+
+    const { info, data } = await sharp(returnedBuffer)
+        .resize(size, size, {
+            fit: "inside",
+        })
+        .ensureAlpha()
+        .raw()
+        .toBuffer({
+            resolveWithObject: true,
+        });
+
+
+    const encoded = encode(
+        new Uint8ClampedArray(data),
+        info.width,
+        info.height,
+        4,
+        4
+    );
+
+    const output: IOutput = {
+        encoded,
+        width,
+        height,
+    };
+
+    return JSON.parse(JSON.stringify(output));
+};
