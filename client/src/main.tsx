429ec3d6afab498903c15271de83ca1f34928a44
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-05-02 09:31:22 -0500

    feat: Added Analytics component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import App from './App'
-import './index.css'
-import { store } from './store/store'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./index.css";
+import { store } from "./store/store";
+import { Analytics as VercelAnalytics } from "@vercel/analytics/react"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  // <React.StrictMode>
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
-  // </React.StrictMode>
-)
+    <VercelAnalytics />
+  </React.StrictMode>
+);
