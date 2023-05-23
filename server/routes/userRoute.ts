f7c0a63d037949ffcd99790f52bf181f7321624a
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-05-22 21:57:26 -0500

    feat: Create endpoint for private access to history

--- server/routes/userRoute.ts
+++ server/routes/userRoute.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { getUser } from "../controllers/user";
+import { getHistory, getUser } from "../controllers/user";
+import { isAuthenticated } from "../middlewares/isAuthenticated";
+import { validate } from "../middlewares/validate";
 
 const userRouter = Router();
 
 userRouter.get('/:slug', getUser)
 
-
+userRouter.get('/history',[
+    isAuthenticated,
+    validate
+],getHistory)
 
 export default userRouter
